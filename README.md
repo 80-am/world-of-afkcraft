96b867a4aef60ec7da87851ceb5f08c30ee72b3a
80-am <adam@flonko.com>
2019-08-27 17:13:42 +0200

    adds bot

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# world-of-afkcraft
-Simple AFK bot for World of Warcraft.
+# World of Afkcraft
+--------------
+
+Simple AFK bot for World of Warcraft
+
+## Prerequisites
+--------------
+
+python3
+pyautogui
+
+## Instructions
+--------------
+
+Open terminal
+
+Checkout repository
+```
+git clone https://github.com/80-am/world-of-afkcraft.git
+```
+
+Start with
+```
+./world-of-afkcraft/world-of-afkcraft.py
+```
+
+## Disclaimer
+--------------
+
+This bot might interfere with Blizzards policy against Third-Party software.
+Using it might suspend your account.
+
+You assume all responsibility and liability.
+
---
+++ world-of-afkcraft.py
@@ -0,0 +1,46 @@
+import time
+import random
+
+from random import randint
+from pyautogui import press
+
+move_keys = ['w', 'a', 'd', 'space']
+print('[1] Random movement')
+print('[2] Attack mode')
+mode = int(input('Select afk-mode: '))
+run = True
+
+print('Focus on WoW window')
+for i in reversed(range(10)):
+    print('Starting in ' + str(i) + ' seconds!')
+    time.sleep(1)
+
+while (run):
+    time_between_actions = randint(1, 24)
+    inner_sleep = random.random()
+    actions = randint(3, 12)
+
+    if (mode == 1):
+        for i in range(actions):
+            press('w')
+        for i in range(actions):
+            random_key = [move_keys[random.randrange(len(move_keys))]
+                    for key in range(4)]
+            press(random_key)
+            time.sleep(inner_sleep)
+        print('Waiting ' + str(time_between_actions) + ' seconds for next action')
+        time.sleep(time_between_actions)
+    
+    if (mode == 2):
+        for i in range(actions):
+            press('tab')
+            press('t')
+            for i in range(5):
+                attack_speed = randint(2, 5)
+                press('1')
+                print('Attacking in ' + str(attack_speed) + ' seconds')
+                time.sleep(attack_speed)
+            
+        print('Waiting ' + str(time_between_actions) + ' seconds for next action')
+        time.sleep(time_between_actions)
+
