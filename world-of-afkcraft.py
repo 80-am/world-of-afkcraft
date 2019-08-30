d8f1152dc1f50766db300ffad5400508b6fb4823
80-am <adam@flonko.com>
2019-08-30 14:14:29 +0200

    adds timing and smoother movement

--- world-of-afkcraft.py
+++ world-of-afkcraft.py
@@ -1,28 +1,34 @@
-import time
+import datetime
+import pyautogui
 import random
+import time
 
 from random import randint
 from pyautogui import press
 
-move_keys = ['w', 'a', 'd', 'space']
+move_keys = ['a', 'd', 'space']
+auto_run_toggle = 'backspace'
 run = True
 
 print('Focus on WoW window')
-for i in reversed(range(10)):
+for i in reversed(range(5)):
     print('Starting in ' + str(i + 1) + ' seconds!')
     time.sleep(1)
 
 while (run):
     time_between_actions = randint(100, 900)
     inner_sleep = random.random()
-    actions = randint(3, 12)
+    actions = randint(1, 6)
+    press(auto_run_toggle)
 
-    for i in range(actions):
-        press('w')
     for i in range(actions):
         random_key = [move_keys[random.randrange(len(move_keys))]
                 for key in range(4)]
         press(random_key)
         time.sleep(inner_sleep)
+
+    press('w')
+    print(datetime.datetime.now().time())
     print('Waiting ' + str(time_between_actions) + ' seconds for next action')
     time.sleep(time_between_actions)
+
